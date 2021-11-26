6463c853e3f53beb1f64cbd43244c86f8b5bcf0f
AnhMHNguyen <84159499+AnhMHNguyen@users.noreply.github.com>
2021-11-26 00:49:26 -0800

    commit

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyACSrVssoh3EHs0dYCr3A3ZZ2GDzbxfP44",
-  authDomain: "clothing-web-83069.firebaseapp.com",
-  projectId: "clothing-web-83069",
-  storageBucket: "clothing-web-83069.appspot.com",
-  messagingSenderId: "1001540559199",
-  appId: "1:1001540559199:web:f4b7d1db645fa13137d6ea",
-  measurementId: "G-Q79D57J2CN"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE,
+  messagingSenderId: process.env.REACT_APP_SENDER,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_ID_MEASUREMENT
 };
 
 const app = firebase.initializeApp(config);
